00cd0b6d0536f779bc749479d34725e49c08afc1
GhoZaa <ghoza16m@gmail.com>
2022-09-10 16:37:40 +0700

    define routes for history

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,7 +1,9 @@
 module.exports = app => {
     const ebook = require("../controllers/controller.js");
+    const history = require("../controllers/history-controller.js");
     var router = require("express").Router();
 
+    // EBOOK
     // Add new ebook item
     router.post("/ebook/", ebook.create);
 
@@ -19,6 +21,25 @@ module.exports = app => {
 
     // Delete all ebook
     router.delete("/ebook/", ebook.deleteAll);
+
+    // HISTORY
+    // Add new history item
+    router.post("/history/", history.create);
+
+    // Retrieve all history
+    router.get("/history/", history.findAll);
+
+    // Retrieve a single history with uuid
+    router.get("/history/:uuid", history.findOne);
+
+    // Update a history with uuid
+    router.put("/history/:uuid", history.update);
+
+    // Delete a history with uuid
+    router.delete("/history/:uuid", history.delete);
+
+    // Delete all history
+    router.delete("/history/", history.deleteAll);
 
     app.use('/api', router);
 };
